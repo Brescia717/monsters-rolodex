d8c05b4633cbabef47d722f75b7970580cea0fd1
Paul Brescia <brescia717@gmail.com>
2022-12-02 15:06:25 -0500

    Implement useEffect Hooks:
    * The App.js file is now a functional component with useEffect hooks. Essentially, it will now
      function the same as the class component version did.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// import React, { Component } from 'react';     * This is for class components.
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
@@ -7,16 +6,28 @@ import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
-  // `this.setState` belongs to class based components, not functional components
-  console.log('render');
-  const [searchField, setSearchField] = useState('a'); // [value, setValue]
-  console.log(searchField)
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  // (callback, dependencies)
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => setMonsters(users))  
+  }, [])
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField])
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString)
   }
-  
   
   return (
     <div className="App">
@@ -26,62 +37,9 @@ const App = () => {
         onChangeHandler={onSearchChange} 
         placeholder='search monsters'
       />
-      {/* <CardList monsters={filteredMonsters} /> */}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
-
-// class App extends Component {
-
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       monsters: [],
-//       searchField: ''
-//     }
-//   }
-
-//   componentDidMount() {
-//     fetch('https://jsonplaceholder.typicode.com/users')
-//       .then(response => response.json())
-//       .then(users => this.setState(() => {
-//         return {monsters: users}
-//       }
-//       ));
-//   }
-
-//   onSearchChange = (event) => {
-//     const searchField = event.target.value.toLowerCase();
-    
-//     this.setState(() => {
-//       // This is syntactic sugar of ES6 => naming the constant the name of an existing key in the `state` (this.searchField) 
-//       // implies that the key is what the variable name is (searchField) and the value is 
-//       // whatever the `searchField` variable's value is: `{searchField: event.target.value.toLowerCase()}
-//       return { searchField };
-//     })
-//   }
-
-//   render() {
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-
-//     const filteredMonsters = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     })
-
-//     return (
-//       <div className="App">
-//         <h1 className='app-title'>Monster Rolodex</h1>
-//         <SearchBox
-//           className='monsters-search-box'
-//           onChangeHandler={onSearchChange} 
-//           placeholder='search monsters'
-//         />
-//         <CardList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
